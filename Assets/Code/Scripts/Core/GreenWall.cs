417d975d63a3c2c82a4476a1027f7cee3939b3c6
Viacheslav <surzhenkoviacheslav@gmail.com>
2022-10-10 18:22:38 +0300

    Hole

--- Assets/Code/Scripts/Core/GreenWall.cs
+++ Assets/Code/Scripts/Core/GreenWall.cs
@@ -8,14 +8,15 @@ namespace SVell
 		[SerializeField] private Transform wall;
 
 		[Header("Bounce")] 
-		[SerializeField] private float moveLength = -0.1f;
-		[SerializeField] private float moveDuration = 1f;
+		[SerializeField] private float moveLength = -0.25f;
+		[SerializeField] private float moveInDuration = 0.1f;
+		[SerializeField] private float moveOutDuration = 1f;
 
 		protected override void OnPlayerCollide()
 		{
-			wall.DOMove(wall.position - wall.forward * moveLength, moveDuration / 2).OnComplete(() =>
+			wall.DOMove(wall.position - wall.forward * moveLength, moveInDuration / 2).OnComplete(() =>
 			{
-				wall.DOMove(wall.position + wall.forward * moveLength, moveDuration).SetEase(Ease.OutBounce);
+				wall.DOMove(wall.position + wall.forward * moveLength, moveOutDuration);
 			});
 		}
 	}
